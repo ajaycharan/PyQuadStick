9da481ecf0f3238e0f5ba6f307790c754ca285fe
Simon D. Levy <simon.d.levy@gmail.com>
2015-02-01 19:50:11 -0500

    Needed only single AXIS_MIN

--- quadstick/axial/rc/__init__.py
+++ quadstick/axial/rc/__init__.py
@@ -16,9 +16,6 @@ rc/__init__.py - Python class for polling R/C transmitters using
 
 '''
 
-
-
-
 from quadstick.axial import Axial
 import os
 
@@ -28,40 +25,36 @@ class RC(Axial):
 
     def __init__(self, name, jsid=0, hidden=False):
         '''
-        Creates a new RC object.
+        Creates a new RC object.  To use this with a real R/C transmitter, you should:
+
+        (1) Move all sticks to center, run qstest.py, and adjust the trims to get a centered (blue) readout.
+
+        (2) If necessary, adjust the self.AXIS_MIN value below to compensate inadequate minimum value.
         '''
         Axial.__init__(self, name, jsid, hidden)
 
         # Empirically determined for Wailly cable + Frsky Taranis
-        self.PITCH_RANGE    = -0.711365, +0.896881
-        self.ROLL_RANGE     = -0.711365, +0.989685
-        self.YAW_RANGE      = -0.711365, +0.958740
-        self.THROTTLE_RANGE = -0.711365, +0.999969
-
+        self.AXIS_MIN = -0.711365
 
     def _get_pitch(self):
-    
-        return self.pitch_sign * self._get_rc_axis(self.pitch_axis, self.PITCH_RANGE)
+
+        return self.pitch_sign * self._get_rc_axis(self.pitch_axis)
 
     def _get_roll(self):
     
-        return self.roll_sign * self._get_rc_axis(self.roll_axis, self.ROLL_RANGE)
+        return self.roll_sign * self._get_rc_axis(self.roll_axis)
+        
 
     def _get_yaw(self):
 
-        return self.yaw_sign * self._get_rc_axis(self.yaw_axis, self.YAW_RANGE)
- 
+        return self.yaw_sign * self._get_rc_axis(self.yaw_axis)
+
     def _get_throttle(self):
         
-        return self._get_rc_axis(self.throttle_axis, self.THROTTLE_RANGE)
+        return self._get_rc_axis(self.throttle_axis)
 
-    def _get_rc_axis(self, value, valrange):
+    def _get_rc_axis(self, index):
         
-        value = Axial._get_axis(self, value) 
-
-        if value > 0:
-            value = value / valrange[1]
-        if value < 0:
-            value = value / abs(valrange[0])
-
-        return value
+        value = Axial._get_axis(self, index) 
+ 
+        return value/abs(self.AXIS_MIN) if value < 0 else value
